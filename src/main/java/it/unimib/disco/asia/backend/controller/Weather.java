637d9aa44b33dc2e188c7c3b5e028f24e2b3d6b1
Unknown <v.cutrona1@gmail.com>
2018-08-09 22:00:13 +0200

    Enable CORS for weather controller

--- src/main/java/it/unimib/disco/asia/backend/controller/Weather.java
+++ src/main/java/it/unimib/disco/asia/backend/controller/Weather.java
@@ -6,12 +6,14 @@ import java.util.Date;
 import it.unimib.disco.asia.backend.model.WeatherObservation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.unimib.disco.asia.backend.repository.WeatherObservationRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class Weather {
 
