189ea0112a202cf79fd3a9c00ee9ab31b8a09d47
Unknown <v.cutrona1@gmail.com>
2020-02-21 18:31:26 +0100

    Limit the number of keywords returned by the keycluster extension

--- src/main/java/it/unimib/disco/asia/backend/controller/CategoryClusters.java
+++ src/main/java/it/unimib/disco/asia/backend/controller/CategoryClusters.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class CategoryClusters {
@@ -17,13 +18,27 @@ public class CategoryClusters {
         this.connector = connector;
     }
 
-    @RequestMapping(value = "keywordscategories", produces = "application/json;charset=UTF-8")
-    public List<KeywordCategories> getWeather(@RequestParam(value = "kws") String keywords) throws Exception {
-        //as input we get comma-separated keywords
-//        System.out.println(keywords);
-        return connector.getCategoriesForMultiKeywords(keywords);
-
+    private KeywordCategories limit(KeywordCategories keywordCategories, int limit) {
+        if (limit < keywordCategories.getCategories().size()) {
+            keywordCategories.setCategories(keywordCategories.getCategories().subList(0, limit));
+        }
+        return keywordCategories;
     }
 
+    @RequestMapping(value = "keywordscategories", produces = "application/json;charset=UTF-8")
+    public List<KeywordCategories> getCategories(@RequestParam(value = "kws") String keywords,
+                                                 @RequestParam(defaultValue = "10") String limit) {
+        int lim = 10;
+        try {
+            lim = Integer.parseInt(limit);
+        } catch (Exception ignored) { }
+
+        List<KeywordCategories> res = connector.getCategoriesForMultiKeywords(keywords);
+        if (lim > 0) {
+            int finalLim = lim;
+            return res.stream().map(keywordCategories -> this.limit(keywordCategories, finalLim)).collect(Collectors.toList());
+        }
+        return res;
+    }
 
 }
